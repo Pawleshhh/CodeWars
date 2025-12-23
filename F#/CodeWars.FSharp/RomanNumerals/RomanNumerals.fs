880f439b0e960f007c41b517144c511254282c35
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2025-12-23 22:49:04 +0100

    Roman 2

--- F#/CodeWars.FSharp/RomanNumerals/RomanNumerals.fs
+++ F#/CodeWars.FSharp/RomanNumerals/RomanNumerals.fs
@@ -5,26 +5,31 @@
         let solution(num: int): String =
             
             let numStr = string num
+            let romanChars =
+                Map [
+                    "M", 1000
+                    "D", 500
+                    "C", 100
+                    "L", 50
+                    "X", 10
+                    "V", 5
+                    "I", 1
+                ]
+
+            let buildNumber (x: string)
 
             let toRomanNum (index: int) (c: char): String =
-            
+
                 let ci = c |> int
 
                 if ci = 0 then ""
                 else
-                    let get4thDecimal () =
-                        new String('M', ci)
-
-                    let get3rdDecimal () =
-                        ""
-                    
-                    let getNumPlace = numStr.Length - index - 1
-                    match index with
-                    | 3 -> get4thDecimal ()
+                    match (numStr.Length - index - 1) with
+                    | 3 -> string (romanChars["M"] * 3)
                     | 2 -> get3rdDecimal ()
                     | _ -> ""
 
             numStr
             |> Seq.toArray
             |> Array.mapi toRomanNum
-            |> String
+            |> String.concat ""
