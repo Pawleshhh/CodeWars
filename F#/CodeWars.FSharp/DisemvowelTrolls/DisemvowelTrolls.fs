652dcd289cd0674b4d471a9b34e0192852be79fd
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2025-12-23 11:17:29 +0100

    Add FSharp project with first task

---
+++ F#/CodeWars.FSharp/DisemvowelTrolls/DisemvowelTrolls.fs
@@ -0,0 +1,10 @@
+﻿module CodeWars.FSharp.DisemvowelTrolls
+
+    let disemvowel (s: string): string =
+      let vowels = ['a'; 'A'; 'e'; 'E'; 'i'; 'I'; 'o'; 'O'; 'u'; 'U']
+
+      s
+      |> Seq.toList
+      |> List.filter (fun c -> List.contains c vowels |> not)
+      |> List.toArray
+      |> System.String
