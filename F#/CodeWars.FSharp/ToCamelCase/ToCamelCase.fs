b172eb2e657dfa50781bc193fb13ed287a85b8d3
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2025-12-23 15:09:11 +0100

    toCamelCase

---
+++ F#/CodeWars.FSharp/ToCamelCase/ToCamelCase.fs
@@ -0,0 +1,17 @@
+﻿module CodeWars.FSharp.ToCamelCase
+
+    let toCamelCase (text: string) =
+        let isSeparator c = c = '-' || c = '_'
+
+        let toUpperCase (c: char) = c |> System.Char.ToUpperInvariant
+
+        text
+        |> Seq.fold (fun s c ->
+            match s with
+            | (l, _) when isSeparator c -> (l @ [], true)
+            | (l, b) when b -> (l @ [toUpperCase c], false)
+            | (l, _) -> (l @ [c], false))
+            (List.Empty, false)
+        |> fst
+        |> List.toArray
+        |> System.String
