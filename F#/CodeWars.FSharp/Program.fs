962b91a97f4df213d906e325e756bdfcf1500bfd
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2025-12-24 12:47:57 +0100

    Roman Numerals

--- F#/CodeWars.FSharp/Program.fs
+++ F#/CodeWars.FSharp/Program.fs
@@ -1,2 +1,6 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
+let x = CodeWars.FSharp.RomanNumerals.solution 1666
+
+printfn "%s" x
--- F#/CodeWars.FSharp/RomanNumerals/RomanNumerals.fs
+++ F#/CodeWars.FSharp/RomanNumerals/RomanNumerals.fs
@@ -2,34 +2,35 @@
 
     open System 
 
-        let solution(num: int): String =
+    let solution(num: int): String =
             
-            let numStr = string num
-            let romanChars =
-                Map [
-                    "M", 1000
-                    "D", 500
-                    "C", 100
-                    "L", 50
-                    "X", 10
-                    "V", 5
-                    "I", 1
-                ]
-
-            let buildNumber (x: string)
-
-            let toRomanNum (index: int) (c: char): String =
-
-                let ci = c |> int
-
-                if ci = 0 then ""
+        let decList =
+            let rec createDecList li n =
+                if n <= 0 then li
                 else
-                    match (numStr.Length - index - 1) with
-                    | 3 -> string (romanChars["M"] * 3)
-                    | 2 -> get3rdDecimal ()
-                    | _ -> ""
+                    createDecList (li @ [ n % 10 ]) (n / 10)
 
-            numStr
-            |> Seq.toArray
-            |> Array.mapi toRomanNum
-            |> String.concat ""
+            createDecList [] num
+            |> List.rev
+
+        let toRomanNum (index: int) (n: int): String =
+            
+            let buildRomanNum (r1: string) (r2: string) (r3: string) =
+                match n with
+                | 9 -> r3 + r1
+                | _ when n > 5 -> r2 + (new String(char r3, n - 5))
+                | 5 -> r2
+                | 4 -> r3 + r2
+                | _ when n > 0 -> new String(char r3, n)
+                | _ -> ""
+                    
+            match (decList.Length - index - 1) with
+            | 3 -> new String ('M', n)
+            | 2 -> buildRomanNum "M" "D" "C"
+            | 1 -> buildRomanNum "C" "L" "X"
+            | 0 -> buildRomanNum "X" "V" "I"
+            | _ -> ""
+
+        decList
+        |> Seq.mapi toRomanNum
+        |> String.concat ""
